12ad7216530560fed4f499b7779d6f4affe4efb5
athiels <arne.thiels@gmail.com>
2016-03-17 12:02:32 +0100

    met link naar results

--- AlmostThere/views/poll.php
+++ AlmostThere/views/poll.php
@@ -7,12 +7,14 @@ trigger_error('views/poll-html.php noodzaakt een $pollData object');
 return "
 <div id='main'>
  <form method='post' action='index.php'>
- <p>$pollData->poll_vraag</p>
- 
+
  <label>Form your yes-no question below</label>
  <br />
  <input class='input' type='text' name='question'  />
  <button class='button' type='submit' value='post'>Create Poll !</button>
+ 
+ <a href='#' ><button class='results'>Go to results</button></a>
  </form>
 
-</div> ";
+</div> 
+";
